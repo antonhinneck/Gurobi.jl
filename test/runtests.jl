2803ca2652b633b3a0423a802bac72d431f9e38e
odow <odow003@aucklanduni.ac.nz>
2018-04-27 16:27:59 +1200

    Passing these tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,31 +1,31 @@
 using Base.Test
 
-# const mpb_tests = [
-#     "lp_01a",
-#     "lp_01b",
-#     "lp_02",
-#     "lp_03",
-#     "lp_04",
-#     "mip_01",
-#     "qp_01",
-#     "qp_02",
-#     "qcqp_01",
-#     "mathprog",
-#     "test_grb_attrs",
-#     "env",
-#     "range_constraints",
-#     "test_get_strarray",
-#     "large_coefficients",
-#     "multiobj"
-# ]
-#
-# @testset "MathProgBase Tests" begin
-#     for t in mpb_tests
-#         fp = "$(t).jl"
-#         println("running $(fp) ...")
-#         evalfile(joinpath("MathProgBase", fp))
-#     end
-# end
+const mpb_tests = [
+    "lp_01a",
+    "lp_01b",
+    "lp_02",
+    "lp_03",
+    "lp_04",
+    "mip_01",
+    "qp_01",
+    "qp_02",
+    "qcqp_01",
+    "mathprog",
+    "test_grb_attrs",
+    "env",
+    "range_constraints",
+    "test_get_strarray",
+    "large_coefficients",
+    "multiobj"
+]
+
+@testset "MathProgBase Tests" begin
+    for t in mpb_tests
+        fp = "$(t).jl"
+        println("running $(fp) ...")
+        evalfile(joinpath("MathProgBase", fp))
+    end
+end
 
 const moi_tests = [
     "mathoptinterface_tests"
